34d465ea6b8dd6fce3251b021eafe2d78627a770
Chris Wiley <sithlord2k@gmail.com>
2025-09-12 16:42:01 -0400

    Added a theme and modified the Home lab page

---
+++ WileySoftwareMudBlazor/Components/Layout/CodeCipherTheme.cs
@@ -0,0 +1,61 @@
+﻿using MudBlazor;
+
+namespace WileySoftwareMudBlazor.Components.Layout
+{
+    public class CodeCipherTheme : MudTheme
+    {
+        public CodeCipherTheme()
+        {
+            PaletteLight = new PaletteLight()
+            {
+                Primary = "#007BFF",
+                PrimaryContrastText = "#0A192F",
+                Background = "#0A192F",
+                Surface = "#112240",
+                TextPrimary = "#CCD6F6",
+                TextSecondary = "#8892B0",
+                AppbarBackground = "#0A192F",
+                AppbarText = "#CCD6F6",
+                LinesDefault = "#303C55",
+                ActionDefault = "#007BFF"
+            };
+
+            Typography = new Typography()
+            {
+                Default = new DefaultTypography()
+                {
+                    FontFamily = new[] { "Inter", "sans-serif" }
+                },
+                H1 = new H1Typography()
+                {
+                    FontFamily = new[] { "Fira Code", "monospace" }
+                },
+                H2 = new H2Typography()
+                {
+                    FontFamily = new[] { "Fira Code", "monospace" }
+                },
+                H3 = new H3Typography()
+                {
+                    FontFamily = new[] { "Fira Code", "monospace" }
+                },
+                H4 = new H4Typography()
+                {
+                    FontFamily = new[] { "Fira Code", "monospace" }
+                },
+                H5 = new H5Typography()
+                {
+                    FontFamily = new[] { "Fira Code", "monospace" }
+                },
+                H6 = new H6Typography()
+                {
+                    FontFamily = new[] { "Fira Code", "monospace" }
+                },
+            };
+
+            LayoutProperties = new LayoutProperties()
+            {
+                DefaultBorderRadius = "6px"
+            };
+        }
+    }
+}
